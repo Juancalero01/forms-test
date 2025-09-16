6bf4770dc13187153e73c4a8676580f8945d7f0c
juancalero <marcelocalero5@gmail.com>
2025-09-16 12:41:14 -0300

    dev

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   formGroups: { [formName: string]: FormGroup } = {};
   fieldsByForm: { [formName: string]: any[] } = {};
-  roleName: string = 'admin';
+  roleName: string = 'tech-A';
   formsLoaded = false;
 
   constructor(private fb: FormBuilder, private http: HttpClient) {}
@@ -19,9 +19,8 @@ export class AppComponent {
       .get(`http://localhost:3000/form-group/1?role=${this.roleName}`)
       .subscribe((data: any) => {
         data.forms.forEach((form: any) => {
-          const fields = form.fields.filter((f: any) => f.canRead);
-          this.fieldsByForm[form.formName] = fields;
-          this.formGroups[form.formName] = this.buildForm(fields);
+          this.fieldsByForm[form.formName] = form.fields;
+          this.formGroups[form.formName] = this.buildForm(form.fields);
         });
 
         this.formsLoaded = true;
@@ -31,8 +30,11 @@ export class AppComponent {
   buildForm(fields: any[]): FormGroup {
     const group: { [key: string]: any } = {};
     fields.forEach((field) => {
-      group[field.name] = [{ value: '', disabled: !field.canEdit }];
+      const disabled = !field.canEdit || !field.canRead;
+
+      group[field.name] = [{ value: field.value || '', disabled }];
     });
+
     return this.fb.group(group);
   }
 
